5ba16f43327864bcc501cf9f134e00fc9d84e0cc
Priyanshu <143485352+quincyypromes@users.noreply.github.com>
2026-04-23 15:00:56 +0530

    Delete Exercise-6 directory

--- Exercise-6/models/books.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const bookSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    available: Boolean
-});
-
-module.exports = mongoose.model('Book', bookSchema);
