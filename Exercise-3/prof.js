8dad3025f793b6220ee12f9942630729cc40fe56
Priyanshu <143485352+quincyypromes@users.noreply.github.com>
2026-04-23 15:00:26 +0530

    Delete Exercise-3 directory

--- Exercise-3/prof.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const professorSchema = new mongoose.Schema({
-    name: String,
-    departments: [String]
-});
-
-module.exports = mongoose.model('Professor', professorSchema);
